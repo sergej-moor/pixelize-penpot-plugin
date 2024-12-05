a2571b16a4c4d2759f0f92d0a38ea3569bd0276c
sergej-moor <104356111+sergej-moor@users.noreply.github.com>
2024-12-05 18:40:20 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Controls from './components/Controls.svelte';
+  import Preview from './components/Preview.svelte';
+  import { theme, updateTheme } from './stores/theme';
+  
+  const handleMessage = (event: MessageEvent) => {
+    if (event.data.type === 'theme') {
+      updateTheme(event.data.content);
+    }
+  }
+</script>
+
+<svelte:window onmessage={handleMessage} />
+
+<main data-theme={$theme}>
+  
+  
+  <div class="flex flex-col gap-2">
+   
+  <Preview></Preview>
+  <Controls></Controls>
+  </div>
+  
+</main>
+
+<style>
+  main {
+    padding: var(--spacing-8);
+  }
+</style>
+
+
+
